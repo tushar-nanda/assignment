464801e144de5f40d8e7bf9f114e81515d99a6b1
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2022-07-25 09:54:42 +0530

    Add files via upload

---
+++ assignment5.c
@@ -0,0 +1,127 @@
+// 1. Write a program to print MySirG n times on the screen
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         printf("MySirG\n");
+//     }
+    
+//     return 0;
+// }
+
+// 2. Write a program to print the first n natural numbers.
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         printf("%d\n",i);
+//     }  
+//     return 0;
+// }
+
+// 3. Write a program to print the first n natural numbers in reverse order
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = n; i >= 1; i--)
+//     {
+//         printf("%d\n",i);
+//     }
+    
+//     return 0;
+// }
+
+// 4. Write a program to print the first n odd natural numbers
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = 1,a=1; i <=n; i++)
+//     {
+//         printf("%d\n",a);
+//         a=a+2;
+//     }
+//     return 0;
+// }
+
+// 5. Write a program to print the first n odd natural numbers in reverse order.
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = n; i >= 1; i--)
+//     {
+//         printf("%d\n",(2*i-1));
+//     }
+    
+//     return 0;
+// }
+
+// 6. Write a program to print the first n even natural numbers
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = 1,a=0; i <=n; i++)
+//     {
+//         printf("%d\n",a);
+//         a=a+2;
+//     }
+    
+//     return 0;
+// }
+
+// 7. Write a program to print the first n even natural numbers in reverse order
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = n; i >= 1; i--)
+//     {
+//         printf("%d\n",(2*i));
+//     }
+    
+//     return 0;
+// }
+
+// 8. Write a program to print squares of the first n natural numbers
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = 1; i <= n; i++)
+//     {
+//         printf("%d\n",i*i);
+//     }
+//     return 0;
+// }
+
+// 9. Write a program to print cubes of the first n natural numbers
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = 1; i <= n; i++)
+//     {
+//         printf("%d\n",i*i*i);
+//     }
+//     return 0;
+// }
+
+// 10. Write a program to print a table of n.
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n); 
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         printf("%d X %d = %d",n,i,n*i);
+//         printf("\n");
+//     }
+//     return 0;
+// }
